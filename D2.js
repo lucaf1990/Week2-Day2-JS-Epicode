fe1e33107ee3eb471d6ddc9b0c7df118f898dde9
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-04-04 22:44:35 +0200

    Update D2.js

--- D2.js
+++ D2.js
@@ -11,65 +11,63 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-/*In progrmmazione con la parola " Tipizzazione" andiamo ad indicare che dobbiamo dare regole differenti ad ogni tipo di dato utilizzato nel codice
-La tipizzazione può essere Diamica o Statica. 
-Variabile Statica rimane strettamente legata al tipo di valore assegnato 
-Variabile Dinamica ovvero nel corso della programmazione possiamo variare il dato asseganto 
-Dato Stringa crea sequenza parole 
-Dato Numerico inserisce in numeri
-Dato Booleano vero e falso*/
+/* Esistono tre tipi di data principali che si distinguono in: 
+ -Numeri,che possono essere di qualsiasi tipo(ES: 2, 4534, -3, 4.346, etc) in quanto java script è un linguaggio considerato di alto livello
+ -Alfanumerici (Stringhe). Sono dati formati dall'insime di numeri e lettere (ES: ''mario'', ''Nome'', ''Nome1'', ''QWERTY'', etc)
+ -Vero/falso (più precisamente dati booleani true/false) che dicono semplicemente se un risultato/una condizione è vera o non vera*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
 let name = "Luca";
-console.log(name);
+console.log("Il mio nome è " +name);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
+let num1 = 4;
+let num2 = 22;
+let sum = num1 + num2;
+console.log("la somma di " + num1 + " e " + num2 + " equivale a: " + sum);
+//
+let sum2 = 4+22;
+console.log(sum2);
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-let num1 = 12;
-let num2 = 20;
-console.log(num1 + num2);
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
 
-/* ESERCIZIO 4 Crea una variable di nome "y" e assegna ad essa il numero 12.*/
-/*
-/* SCRIVI QUI LA TUA RISPOSTA */
-let y = 12;
-console.log(y);
+let x;
+x = 12;
+
+console.log("la variabile x vale: "+ x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
-
-name = "luca";
-
-//const dimostroChe = "la variabile Const non può essere riasseganta";
-/*dimostroChe = "Non si può modificare";
-console.log(dimostroChe); //Assignment to constant variable
+name = "Forma"
+console.log(" 'name' : " + name);
+const y = 5;
+// y = 6; 
+// se si ridichiara o si va a modificare il valore di y il programma si interrompe dando errore.
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
+let sub = x-4;
+console.log("la sottrazione tra " +x+ " e 4 fa: "+ sub);
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-let x = 12;
-console.log(x - 4);
+
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-
-let name4 = "john";
-let name5 = "John";
-console.log(name4 === name5);
-console.log(name4.toLowerCase() === name5.toLocaleLowerCase());
+let name1 = "john";
+let name2 = "John";
+console.log("questi sono i due nome originali: " + name1 +" e "+ name2);
+console.log("Questo è il risultato della comparazione diretta tra le due variabili: ");
+console.log( name1 === name2);
+console.log("Applico 'lowercase' alle variabili otterrò:");
+console.log(name1 === name2.toLowerCase());
